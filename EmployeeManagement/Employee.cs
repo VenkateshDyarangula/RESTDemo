493a14cb9638ccf68c2a797bef4f57d36ffa646a
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-24 19:57:08 +0530

    REST Demo Added

---
+++ EmployeeManagement/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double Salary { get; set; }
+    }
+
+}
